8bb75e6a56d22b68fbb778e09ad005870d6c38d7
Wakabayashi, Kenichi <kwaka1208@users.noreply.github.com>
2016-01-20 10:40:01 +0900

    Add ImageMagick installation

--- dothat.sh
+++ dothat.sh
@@ -6,4 +6,6 @@ echo "#### This shows you have to do at first. ####"
 echo "#### git configuration ####" 
 echo "git config --global user.name 'your name'"
 echo "git config --global user.email 'your-mail@example.com'"
-
+echo "#### install ImageMagick ####" 
+echo "yum -y install libjpeg-devel libpng-devel"
+echo "yum -y install ImageMagick ImageMagick-devel"
