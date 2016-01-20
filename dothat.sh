60fe47d5751518d4e8215e87bcf1a8fdcd2ae77f
Wakabayashi, Kenichi <kwaka1208@pote2.net>
2016-01-20 00:27:45 +0000

    add guidance for configuration

---
+++ dothat.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "#### This shows you have to do at first. ####" 
+
+# git config
+echo "#### git configuration ####" 
+echo "git config --global user.name 'your name'"
+echo "git config --global user.email 'your-mail@example.com'"
+
